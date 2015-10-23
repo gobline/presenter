6c9474a21c380b00b652e04c8a6c22faec034472
Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
2015-10-23 23:57:27 +0300

    Added a simple PresenterFactory implementation

---
+++ src/PresenterFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Mendo Framework
+ *
+ * (c) Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mendo\Presenter;
+
+/**
+ * @author Mathieu Decaffmeyer <mdecaffmeyer@gmail.com>
+ */
+class PresenterFactory implements PresenterFactoryInterface
+{
+    public function createPresenter($subject)
+    {
+        return new Presenter($subject);
+    }
+}
